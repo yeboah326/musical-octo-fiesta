db3a6c109ad388e2e34d53f4e7189176dcb41a63
yeboah326 <asanteg36@gmail.com>
2021-05-01 09:26:40 +0000

    Added database configurations

--- medical_app/medical_app/settings.py
+++ medical_app/medical_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'medical_app_backend',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'medical_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': 'new_medical_app',
+        'USER': 'testuser',
+        'PASSWORD': 'testpassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
